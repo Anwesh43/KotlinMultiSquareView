2f6d8c5ba356b54470351546747d652655c997ea
Anwesh43 <anweshthecool0@gmail.com>
2018-04-15 12:31:25 +0530

    adding fullScreenSupport

--- app/src/main/java/ui/anwesome/com/kotlinmultisquareview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinmultisquareview/MainActivity.kt
@@ -2,6 +2,7 @@ package ui.anwesome.com.kotlinmultisquareview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.multisquareview.MultiSquareView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MultiSquareView.create(this)
+        fullscreen()
     }
+}
+
+fun MainActivity.fullscreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
