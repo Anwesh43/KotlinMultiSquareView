a05973a609d73026868d279ab3984d0ac23d455e
Anwesh43 <anweshthecool0@gmail.com>
2018-04-15 11:25:17 +0530

    adding Animator class

--- multisquareview/src/main/java/ui/anwesome/com/multisquareview/MultiSquareView.kt
+++ multisquareview/src/main/java/ui/anwesome/com/multisquareview/MultiSquareView.kt
@@ -118,6 +118,32 @@ class MultiSquareView (ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator (var view : MultiSquareView, var animated : Boolean = false) {
+        fun animate(updatecb : () -> Unit) {
+            try {
+                updatecb()
+                Thread.sleep(50)
+                view.invalidate()
+            }
+            catch(ex : Exception) {
+
+            }
+        }
+
+        fun start () {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop () {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
 
 fun ConcurrentLinkedQueue<MultiSquareView.SquarePart>.at(j : Int) : MultiSquareView.SquarePart? {
