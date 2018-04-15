5a237150b0eed5610c06777f79e5d14ea005ec05
Anwesh43 <anweshthecool0@gmail.com>
2018-04-15 12:28:04 +0530

    adding MultiSquareView in activity

--- app/src/main/java/ui/anwesome/com/kotlinmultisquareview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinmultisquareview/MainActivity.kt
@@ -2,10 +2,12 @@ package ui.anwesome.com.kotlinmultisquareview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.multisquareview.MultiSquareView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MultiSquareView.create(this)
     }
 }
