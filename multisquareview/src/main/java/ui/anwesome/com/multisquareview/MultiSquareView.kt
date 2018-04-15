9bbe5306165b5e20abaa80a36121811511c115f6
Anwesh43 <anweshthecool0@gmail.com>
2018-04-15 10:35:58 +0530

    adding MultiSquareView class

--- multisquareview/src/main/java/ui/anwesome/com/multisquareview/MultiSquareView.kt
+++ multisquareview/src/main/java/ui/anwesome/com/multisquareview/MultiSquareView.kt
@@ -7,3 +7,21 @@ package ui.anwesome.com.multisquareview
 import android.view.*
 import android.content.*
 import android.graphics.*
+
+class MultiSquareView (ctx : Context) : View(ctx) {
+
+    val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
